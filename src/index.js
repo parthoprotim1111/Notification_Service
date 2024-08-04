77f421db4863a25c7c4120002a12951e91c2674a
parthoprotim1111 <parthoprotim1112@gmail.com>
2024-08-05 00:59:34 +1000

    created nodemailer transport object

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 
-const { ServerConfig } = require('./config');
+const { ServerConfig,emailConfig } = require('./config');
 const apiRoutes = require('./routes');
-
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // to parse JSON bodies
+
+
+
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT,async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
+    try {
+        const res= await emailConfig.sendMail({
+            from: ServerConfig.GMAIL_EMAIL,
+            to: "fotavo6901@alientex.com",
+            subject: "Server Testing",
+            text: "It is working"
+            
+        })
+        console.log(res)
+    
+    } catch (error) {
+        console.log(error)
+        
+    }
+
 });
