45384f68e00f2d92aa42aee3431b638077a65e9f
parthoprotim1111 <parthoprotim1112@gmail.com>
2024-08-06 00:01:33 +1000

    configuired email notification through rabbitmq

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 const express = require('express');
-
+const amqplib = require('amqplib');
 const { ServerConfig,emailConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { emailService } = require('./services');
 const app = express();
+
+
+
+async function connectQueue() {
+    try {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("notification-queue");
+        channel.consume("notification-queue",async (data)=>{
+            console.log(data)
+            console.log(`${Buffer.from(data.content)}`);
+            let object;
+            try {
+                object = JSON.parse(`${Buffer.from(data.content)}`);
+            } catch (error) {
+                channel.ack(data);
+                return;
+                
+            }
+            await emailService.sendEmails(ServerConfig.GMAIL_EMAIL,object.recepientEmail, object.subject,object.text)
+            channel.ack(data);
+        })
+
+    } catch (error) {
+        console.log(error)
+        
+    }
+    
+}
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // to parse JSON bodies
@@ -14,19 +45,22 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT,async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    try {
-        const res= await emailConfig.sendMail({
-            from: ServerConfig.GMAIL_EMAIL,
-            to: "fotavo6901@alientex.com",
-            subject: "Server Testing",
-            text: "It is working"
+    // try {
+    //     const res= await emailConfig.sendMail({
+    //         from: ServerConfig.GMAIL_EMAIL,
+    //         to: "fotavo6901@alientex.com",
+    //         subject: "Server Testing",
+    //         text: "It is working"
             
-        })
-        console.log(res)
+    //     })
+    //     console.log(res)
     
-    } catch (error) {
-        console.log(error)
+    // } catch (error) {
+    //     console.log(error)
         
-    }
+    // }
+    await connectQueue();
+    console.log("Queue is UP")
+
 
 });
